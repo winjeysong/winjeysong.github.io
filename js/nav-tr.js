9563bc943cd09dd46faa2161977a20bf94cd9422
winjeysong <winjey-song@163.com>
2017-07-10 01:12:25 +0800

    Finish the function of nav-transform

--- js/nav-tr.js
+++ js/nav-tr.js
@@ -1,22 +1,14 @@
-
 //customise
-/*$(function(){
-  var a_inner=$("div#foo").find("li");
-  a_inner.on("click",function(){
-    $(".active").removeClass("active");
-   // $(this).addClass("active nav-mark")//.siblings().removeClass("active");
 
-    $("#foo>ul>li").each(function(){
-    $this=$(this);
-    if($this[0].href==String(window.location)){
-      $this.addClass("active");
-    }
+$(function(){
+    $("#foo>ul>li>a:gt(0)").each(function(){
+      var reg = /(http:\/\/)(.*)\/([^\/]*)\//g;
+      var match0 = $(this).text().toLowerCase();
+      var loc = String(window.location);
+      if(reg.exec(loc)[3] == match0){
+        $(this).parent().siblings().removeClass("active");
+        $(this).parent().addClass("active");  
+      }  
   });
-  });
-  $("#foo>ul>li").each(function(){
-    $this=$(this);
-    if($this[0].href==String(window.location)){
-      $this.addClass("active");
-    }
-  });
-});*/
+});
+

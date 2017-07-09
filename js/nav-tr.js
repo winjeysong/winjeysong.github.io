b08ea1552e35cb9820950999c6fb0522ee881c47
winjeysong <winjey-song@163.com>
2017-07-10 01:18:28 +0800

    add commented-out code

--- js/nav-tr.js
+++ js/nav-tr.js
@@ -2,10 +2,10 @@
 
 $(function(){
     $("#foo>ul>li>a:gt(0)").each(function(){
-      var reg = /(http:\/\/)(.*)\/([^\/]*)\//g;
+      var reg = /(http:\/\/)(.*)\/([^\/]*)\//g;  //匹配链接最后/.../斜杠里的内容
       var match0 = $(this).text().toLowerCase();
       var loc = String(window.location);
-      if(reg.exec(loc)[3] == match0){
+      if(reg.exec(loc)[3] == match0){  //如果链接最后斜杠里的内容和a标签里的内容相同，则添加active类，高亮当前正在浏览的页
         $(this).parent().siblings().removeClass("active");
         $(this).parent().addClass("active");  
       }  
